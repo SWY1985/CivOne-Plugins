aa0aabd7d85a917fcb571f374d99292a4c7e8e95
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2017-08-02 15:47:24 +0200

    Removed enclosing Namespace

--- Samples/VB/hello-vb/Plugin.vb
+++ Samples/VB/hello-vb/Plugin.vb
@@ -1,25 +1,23 @@
 Imports CivOne.Interfaces
 
-Namespace CivOne
-    Public Class Plugin
-            Implements IPlugin
-        
-		Public ReadOnly Property Name() As String Implements IPlugin.Name
-			Get
-				Return "Hello VB.NET!"
-			End Get
-		End Property
-        
-		Public ReadOnly Property Author() As String Implements IPlugin.Author
-			Get
-				Return "SWY1985"
-			End Get
-		End Property
-        
-		Public ReadOnly Property Version() As String Implements IPlugin.Version
-			Get
-				Return "0.1.0"
-			End Get
-		End Property
-    End Class
-End Namespace
+Public Class Plugin
+    Implements IPlugin
+
+    Public ReadOnly Property Name As String Implements IPlugin.Name
+        Get
+            Return "Hello VB.NET!"
+        End Get
+    End Property
+
+    Public ReadOnly Property Author As String Implements IPlugin.Author
+        Get
+            Return "SWY1985"
+        End Get
+    End Property
+
+    Public ReadOnly Property Version As String Implements IPlugin.Version
+        Get
+            Return "0.1.0"
+        End Get
+    End Property
+End Class
